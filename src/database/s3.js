e0fd40b90092e610c3a9d979c2dc76798da97628
Jinoo Lee <etture@gmail.com>
2018-12-11 16:03:30 +0900

    hid all S3 configs

--- src/database/s3.js
+++ src/database/s3.js
@@ -6,7 +6,7 @@ const path = require('path');
 aws.config.update({
     secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
     accessKeyId: process.env.S3_ACCESS_KEY_ID,
-    region: 'ap-northeast-2'
+    region: process.env.S3_REGION
 });
 
 const s3 = new aws.S3();
@@ -14,7 +14,7 @@ const s3 = new aws.S3();
 const upload = multer({
     storage: multerS3({
         s3,
-        bucket: 'jinoolee-aws-s3',
+        bucket: process.env.S3_BUCKET,
         key: function(req, file, cb) {
             console.log(file);
             cb(null, Date.now().toString() + path.extname(file.originalname));
